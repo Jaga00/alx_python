0f503c4358abf28a0e1eb4710dba436a258832c7
Jaga00 <jamessimba07@gmail.com>
2023-07-19 16:43:59 +0300

    python

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,3 +1,3 @@
 # Python script that prints exactly "Programming is like building a multilingual puzzle, and followed by a new line.
 
-print(' "Programming is like building a multilingual puzzle')
+print('"Programming is like building a multilingual puzzle')
