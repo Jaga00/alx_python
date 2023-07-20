2f825bb34e494aac5fce295f642e0644c2459ddc
Jaga00 <jamessimba07@gmail.com>
2023-07-20 16:31:01 +0300

    power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,14 @@
 # function that computes a to the power of b and return the value.
 
 def pow(a, b):
-    result = 1
+    if b == 0:
+        return 1
 
+    if b < 0:
+        a = 1 / a
+        b = abs(b)
+
+    result = 1
     for _ in range(b):
         result *= a
 
