2e68244c8bbdf1d0363232f87f0c4723dcaad637
Jaga00 <jamessimba07@gmail.com>
2023-07-21 13:51:11 +0300

    python

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -3,13 +3,11 @@
 def pow(a, b):
     if b == 0:
         return 1
-
-    if b < 0:
-        a = 1 / a
-        b = abs(b)
-
-    result = 1
-    for _ in range(b):
-        result *= a
-
-    return result
+    elif b < 0:
+        return 1 / pow(a, -b)
+    else:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    
