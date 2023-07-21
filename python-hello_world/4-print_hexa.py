30507104af16d23270648414db54a5a85af0d173
Jaga00 <jamessimba07@gmail.com>
2023-07-21 16:16:05 +0300

    python quiz

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,4 +1,4 @@
 # This is a program that prints all numbers from 0 to 98 in decimal and in hexadecimal
 
 for num in range(99):
-    print(f"{num} = 0x{num:X}")
+    print("{:02d} = 0x{:X}".format(num, num))
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@
 
 for tens_digit in range(10):
     for units_digit in range(tens_digit + 1, 10):
-        print(f"{tens_digit:2d}{units_digit:d}", end=",")
+        print("{:2d}{:d}".format(tens_digit, units_digit), end=",")
 
 print()
