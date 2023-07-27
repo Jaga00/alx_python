dfd59840158a66e18a3f05e0f3c29bae0ef1bb13
Jaga00 <jamessimba07@gmail.com>
2023-07-27 14:10:35 +0300

    list

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,7 @@
-# a function that prints a matrix of integers.
+# a function that prints a matrix of integers.s
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end=" ")
+            print("{}".format(element), end=" ")
         print()
