1841998d81da64ec3b397478aa3d175e93b5643c
Jaga00 <jamessimba07@gmail.com>
2023-07-27 15:48:48 +0300

    more data structures

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -2,3 +2,9 @@
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
+
+my_dict = {'name': 'John', 'age': 30}
+update_dictionary(my_dict, 'city', 'New York')  # Add 'city': 'New York'
+update_dictionary(my_dict, 'age', 31)           # Update 'age' to 31
+
+print(my_dict)
